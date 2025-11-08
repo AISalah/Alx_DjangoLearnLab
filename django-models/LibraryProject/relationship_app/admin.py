4ab1accfe9d4d012c0dd9366a385b0351fd58083
AISalah <salah.ais48@gmail.com>
2025-11-08 23:27:40 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# Import all the models from your app's models.py
+from .models import Author, Book, Library, Librarian
 
-# Register your models here.
+# Register each model with the admin site
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
