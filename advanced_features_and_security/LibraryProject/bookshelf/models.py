e37c326340c58faa3623a1c2bee93f9a8be58657
AISalah <salah.ais48@gmail.com>
2025-11-13 00:14:18 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager
+
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+class UserProfile(models.Model):
+    # This now correctly links to whichever user model is active
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, UserManager
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -16,6 +16,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
@@ -23,39 +24,9 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
-
-    def __str__(self):
-        return f'{self.user.username} - {self.role}'
-
-
-class CustomUserManager(UserManager):
-    pass
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
+        return self.user.username
