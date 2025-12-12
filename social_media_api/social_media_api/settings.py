56908a05348872be355ae88ff57e0d6b2159becc
AISalah <salah.ais48@gmail.com>
2025-12-12 15:22:18 +0100

    Add production logging config

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -152,3 +152,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
